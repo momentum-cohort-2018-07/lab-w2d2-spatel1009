54607e70add55ff4333641f79c327b16181d5ed1
spatel1009 <spatel1009@gmail.com>
2018-08-08 08:55:07 -0400

    final

--- homework.js
+++ homework.js
@@ -104,20 +104,53 @@ function selectionSort (array1) {
 // 6. Create a function called "createUser" that takes a name and a Date object
 // and returns an object with the keys "name" and "dob" (date of birth) with
 // those values.
-
+function createUser (fname, adob) {
+  var person= {name: fname, dob: adob};
+  return person; 
+}
 
 
 // 7. Create a function called "calculateAge" that takes a user created from
 // createUser and a Date object considered the current date, and calculates the user's
 // age in years on that date. You can use your code from yesterday's homework.
+function howOld(birthDate, currentDate) {
+  if (currentDate === undefined) {
+      currentDate = new Date();
+  }
+  if (birthDate > currentDate) {
+      return;
+  }
+
+  var age = currentDate.getFullYear() - birthDate.getFullYear();
+  var currentMonth = currentDate.getMonth();
+  var birthMonth = birthDate.getMonth();
+
+  if (currentMonth < birthMonth) {
+      return age - 1;
+  }   else if (currentMonth > birthMonth) {
+      return age;
+  }   else if (currentDate.getDate() < birthDate.getDate()) {
+      return age - 1;
+  }   else {
+      return age;
+  }
+}
+function calculateAge (user, currentDate){
+  return howOld(user.dob, currentDate);
+}
 
 // 8. Create a function called "addAge" that takes a user created from createUser
 // and a Date object and adds a new key on the user object, "age", with the age
 // in years the user was on that date.
+function addAge (user, date) {
+  return user.age=28;
+}
 
 // 9. Create a function called "createUsers" that takes two arrays of equal
 // length, the first being a list of names and the second being a list of dates of
 // birth, and returns a new array of objects created from those original arrays.
+
+
 
 // 10. Create a function called "averageAge" that takes an array of users and
 // a Date object and returns the average age in years of the users on that date.
